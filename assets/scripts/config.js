d02880ec007b262a3e786cbc1fc8aa7ed1585735
omniczech <mikepetschek@gmail.com>
2018-04-26 11:22:30 -0400

    Added new heroku URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://pacific-reaches-42447.herokuapp.com',
+  production: 'https://movie-full-stack-rails.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
